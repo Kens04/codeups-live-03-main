74fbb7f2033cce3d4fc84409a52161d67e026d27
John Doe <johndoe@example.com>
2022-05-31 08:18:30 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  var topBtn = $('.pagetop');
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300, 'swing');
+    return false;
+  });
+
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($('.js-hamburger').hasClass('is-active')) {
+      $('.js-hamburger').removeClass("is-active");
+      // $("html").toggleClass("is-fixed");
+      $(".js-sp-nav").fadeOut(300);
+    } else {
+      $('.js-hamburger').addClass("is-active");
+      // $("html").toggleClass("is-fixed");
+      $(".js-sp-nav").fadeIn(300);
+    }
+  });
+
+
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
+  });
+
+  var swiper = new Swiper(".js-works-swiper", {
+    pagination: {
+      el: ".js-works-pagination",
+    },
+    loop: true,
+    clickable: true,
+    // autoplay: {
+    //   delay: 3000,
+    // },
+  });
+});
